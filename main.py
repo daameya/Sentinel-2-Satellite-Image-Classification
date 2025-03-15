acd03a9de5d88e8f3c13bebe7fe728d0fd2cffa6
daameya <1998ameya@gmail.com>
2025-03-15 16:28:19 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from src.SatelliteImageClassification import logger
+from SatelliteImageClassification import logger
+from SatelliteImageClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom log")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
